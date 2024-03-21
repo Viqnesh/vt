73048ed52ebbd377d8e86d76d59c104de9e2bf0f
Vignesh <vignesht.pro@gmail.com>
2024-03-21 18:12:04 +0100

    ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
   title: 'Vignesh - Portfolio',
   description: 'Portfolio de Vignesh Tillaisababady',
 }
+
 
 export default function RootLayout({
   children,
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-9V0S2MQ677" />
     </html>
   )
 }
